c6e178247c09f3229607cffac54f2966e2350ee9
5820K <38088640+chuynh18@users.noreply.github.com>
2018-05-27 00:29:22 -0700

    update README.md to reflect (optional) compilation of the TS files

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 MySQL mock storefront
 =====================
 
-This is a stubbed `README.md` for my MySQL-powered mock storefront.
+Compilation
+-----------
+For your convenience, compiling the TypeScript source (located in `./src`) is not necessary.  You'll find the compiled JavaScript in `./lib`.  However, if you wish to compile from source, please ensure that you have TypeScript installed.  TypeScript can be installed globally by running `npm install -g typescript`.  Otherwise, you can install it locally.  First, make sure your current working directory is the root directory of this project.  Then simply run `npm install --save-dev typescript`.
+
+Afterward, simply run `tsc`.  The relevant JavaScript files will be generated and placed inside `./lib`.  If there are already files present in `./lib`, they will be overwritten.
--- src/tableMaker.ts
+++ src/tableMaker.ts
@@ -1,29 +1,32 @@
 // functionality for pretty printing text tables in the terminal:
 // fundamentally, what a text table is... is making it so each item in a column has the same length
+
 // so we need to get the length of the longest item in a column, then pad every item with a shorter length the appropriate amount
-export var getLengthOfLongestItem = function (response, col) {
+export var getLengthOfLongestItem = function (response: object[], col: number | string): number {
     var longest = 0;
-    for (var i = 0; i < response.length; i++) {
+    for (var i: number = 0; i < response.length; i++) {
         if (response[i][col].toString().length > longest) {
             longest = response[i][col].toString().length;
         }
     }
     return longest;
 };
+
 // then we need to generate the right number of spaces for everything shorter than the longest item
 // this is the difference between the length of the longest string and the current string we're considering
-export var generatePadding = function (lenOfLongest, lenOfCurrent) {
-    var space = "";
+export var generatePadding = function (lenOfLongest: number, lenOfCurrent: number): string {
+    var space: string = "";
     var difference = lenOfLongest - lenOfCurrent;
-    for (var i = 0; i < difference; i++) {
+    for (var i: number = 0; i < difference; i++) {
         space += " ";
     }
     return space;
 };
+
 // generates horizontal line of correct length
-export var generateHorizontalSeparator = function (howManyChar) {
-    var separator = "";
-    for (var i = 0; i < howManyChar; i++) {
+export var generateHorizontalSeparator = function (howManyChar: number): string {
+    var separator: string = "";
+    for (var i:number = 0; i < howManyChar; i++) {
         separator += "―";
     }
     return separator;
