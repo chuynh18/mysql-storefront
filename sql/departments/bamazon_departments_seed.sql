c9ca99007681206f4f8cff84e478f424954b8390
Acer <38088640+chuynh18@users.noreply.github.com>
2018-05-30 21:56:28 -0700

    fix bamazon_departments_seed.sql

--- sql/departments/bamazon_departments_seed.sql
+++ sql/departments/bamazon_departments_seed.sql
@@ -1,6 +1,9 @@
 USE bamazon;
 
-INSERT INTO departments (product_name, department_name, maker, product_description, price, stock_quantity)
+INSERT INTO departments (department_name, over_head_costs)
 VALUES
-("Nikon D850", "Photography", "Nikon", "Enthusiast level full-frame DSLR camera.", 3299.95, 3),
-("Scalp Massager 2-pack – Handheld Head Massage Tingler", "Health", "", "Feel your stress melt away... fast!", 5.99, 267);
+("Photography", 1234),
+("Music", 1000),
+("Electronics", 2000),
+("Books", 1000),
+("Health", 500);
